9e4cb8e3fbb58347d332636c67a89075209c262d
Afonso Bordado <afonsobordado@az8.co>
2017-06-02 21:03:13 +0000

    Split tilebuffer image split

--- src/map.rs
+++ src/map.rs
@@ -44,8 +44,11 @@ impl TiledMap {
     pub fn new<F: Factory<Resources>>(path: &Path, factory: &mut F) -> Result<TiledMap> {
 
         // This code is a mess, please clean it up
-        let mut pathbuf = path.to_path_buf();
-        pathbuf.pop(); // Pop the map name
+        let mut pathbuf = {
+            let mut p = path.to_path_buf();
+            p.pop(); // Pop the map name
+            p
+        };
 
         let map = parse(File::open(path).unwrap())?;
         let mut texture_buffers = HashMap::new();
@@ -56,24 +59,17 @@ impl TiledMap {
                 tileset.tile_height
             ];
             for ref image in &tileset.images {
-                let mut image_path = pathbuf.clone();
-                image_path.push(image.source.clone());
+                let mut image_path = {
+                    let mut p = pathbuf.clone();
+                    image_path.push(image.source.clone());
+                    p
+                };
+
                 let tb = TileBuffer::new(image_path, tile_dimensions);
-
-                let mut col_vec = vec![];
-                for i in 0..tb.tiles_available()[0] {
-                    let mut row_vec = vec![];
-
-                    for j in 0..tb.tiles_available()[1] {
-                        row_vec.push(tb.texture([i,j], factory).unwrap());
-                    }
-
-                    col_vec.push(row_vec);
-                }
 
                 texture_buffers.insert(
                     image.source.clone(),
-                    col_vec
+                    TiledMap::split_image(tb, factory)
                 );
 
             }
@@ -83,6 +79,20 @@ impl TiledMap {
             map,
             texture_buffers
         })
+    }
+
+    fn split_image<F: Factory<Resources>>(tb: TileBuffer, factory: &mut F) -> Vec<Vec<Rc<G2dTexture>>> {
+        let mut col_vec = vec![];
+        for i in 0..tb.tiles_available()[0] {
+            let mut row_vec = vec![];
+
+            for j in 0..tb.tiles_available()[1] {
+                row_vec.push(tb.texture([i,j], factory).unwrap());
+            }
+
+            col_vec.push(row_vec);
+        }
+        col_vec
     }
 }
 
